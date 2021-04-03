e4f46205c094acf70431a2a76e1324f9d9e155cb
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2021-04-03 11:38:09 +0300

    CI. Adds podman

--- .semaphore/ubuntu1804-podman-install.sh
+++ .semaphore/ubuntu1804-podman-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 source /etc/os-release
 
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ / " > sudo /etc/apt/sources.list.d/podman.list
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+PODMAN_REPO="https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/"
+echo "deb $PODMAN_REPO  / " | sudo tee -a /etc/apt/sources.list.d/podman.list > /dev/null
+curl -L $PODMAN_REPO/Release.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install podman buildah -y
