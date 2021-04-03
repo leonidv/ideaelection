292d6de328e7edccf5ce58729a0624d376a7f876
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2021-04-03 21:34:50 +0300

    CI. Adds podman fixes

--- scripts/couchbase-run.sh
+++ scripts/couchbase-run.sh
@@ -15,7 +15,7 @@ CONTAINER_ID=$(${CONTAINER_MANAGER} ps -a -q --filter name=idel-couchbase)
 
 if [[ -z "${CONTAINER_ID}" ]]
 then
-    ${CONTAINER_MANAGER} run -p 8091-8094:8091-8094 -p 11210:11210 --name idel-couchbase  idel-couchbase
+    ${CONTAINER_MANAGER} run -q -p 8091-8094:8091-8094 -p 11210:11210 --name idel-couchbase  docker.io/leonidv/idel-couchbase
 else
     ${CONTAINER_MANAGER} start idel-couchbase
 fi
