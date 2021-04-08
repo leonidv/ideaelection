e0d9c2fd7247bfe9154d677e2585e0b4094cc9f2
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2021-04-08 22:34:45 +0300

    CI. Clear hdd fix

--- .semaphore/ubuntu1804-podman-install.sh
+++ .semaphore/ubuntu1804-podman-install.sh
@@ -4,5 +4,7 @@ source /etc/os-release
 PODMAN_REPO="https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/"
 echo "deb $PODMAN_REPO  / " | sudo tee -a /etc/apt/sources.list.d/podman.list > /dev/null
 curl -L $PODMAN_REPO/Release.key | sudo apt-key add -
-sudo apt-get -q update
-sudo apt-get -q install podman buildah -y
+sudo apt-get -qq update
+sudo apt-get -qq install podman -y
+
+mkdir "$HOME/.config"
