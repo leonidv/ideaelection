6133daf009562c8f2ae7fa4e5d5401c0161efad9
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2021-04-09 07:39:07 +0300

    CI. check that idel-backend.jar is exists before run build image

--- deploy/build-backend.sh
+++ deploy/build-backend.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+APP_JAR="../backend/build/libs/idel-backend.jar"
 MODE=${1:-dev}
 echo "mode is ${MODE}"
+
+
+if [[ ! -f "$APP_JAR" ]]
+then
+  echo "can't read application jar"
+  echo "APP_JAR=$APP_JAR"
+  exit 1
+fi
 
 podman build --quiet -t docker.io/leonidv/idel-backend-${MODE} -f backend/Containerfile \
   --build-arg PROJECT_DIR=backend \
