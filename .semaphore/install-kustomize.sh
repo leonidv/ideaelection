312d4b78ff53768839f688c5bbea749cc23af968
Leonid Vygovskiy <leonid.vygovskiy@gmail.com>
2021-05-02 18:40:20 +0300

    Add probes and build info

--- .semaphore/install-kustomize.sh
+++ .semaphore/install-kustomize.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo mv kustomize /usr/bin/kustomize
 kustomize version
