85afd975717df99628b552bb2908f0fe15636eef
ptrhta <69871999+tptrhta@users.noreply.github.com>
2022-05-12 15:57:35 +0300

    npm audit
    https://cp.niceneasy.ru/issues/19287

--- webui/webpack.config.js
+++ webui/webpack.config.js
@@ -27,7 +27,9 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   devServer: {
-    contentBase: path.join(__dirname, "src"),
+    static: {
+      directory: path.join(__dirname, "src")
+    },
     historyApiFallback: true,
   },
   module: {
