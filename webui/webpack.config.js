46dc918166ac6c9d3c7a7bf7963e53be368c70a8
ptrhta <69871999+tptrhta@users.noreply.github.com>
2022-06-03 00:28:58 +0300

    change webpack

--- webui/webpack.config.js
+++ webui/webpack.config.js
@@ -1,106 +1,121 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const { GitRevisionPlugin } = require("git-revision-webpack-plugin");
-//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const gitRevisionPlugin = new GitRevisionPlugin();
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const gitRevisionPlugin = new GitRevisionPlugin()
+const CompressionPlugin = require('compression-webpack-plugin')
 
-const d = new Date(gitRevisionPlugin.lastcommitdatetime());
+const d = new Date(gitRevisionPlugin.lastcommitdatetime())
 const dateInfo =
   d.getFullYear() +
-  "-" +
-  ("0" + (d.getMonth() + 1)).slice(-2) +
-  "-" +
-  ("0" + d.getDate()).slice(-2);
-const versionInfo = gitRevisionPlugin.version();
+  '-' +
+  ('0' + (d.getMonth() + 1)).slice(-2) +
+  '-' +
+  ('0' + d.getDate()).slice(-2)
+const versionInfo = gitRevisionPlugin.version()
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "index.bundle.[chunkhash:4].js",
-    publicPath: "/",
-    clean: true,
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.[chunkhash:4].js',
+    publicPath: '/',
+    clean: true
   },
-  devtool: false,
+  devtool: 'source-map',
   optimization: {
+    removeAvailableModules: true,
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false
+          }
+        },
+        extractComments: false,
+        parallel: true
+      })
+    ],
     splitChunks: {
-      chunks: 'all',
-    },
+      chunks: 'all'
+    }
   },
-  mode: process.env.NODE_ENV || "production",
+  mode: process.env.NODE_ENV || 'production',
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js']
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "src"),
+      directory: path.join(__dirname, 'src')
     },
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader']
       },
       {
         test: /\.m?js/,
         resolve: {
-          fullySpecified: false,
-        },
+          fullySpecified: false
+        }
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["ts-loader"],
+        use: ['ts-loader']
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
-    {
+      {
         test: /\.(woff|woff2|ttf|otf|eot)$/,
         type: 'asset/resource',
         generator: {
           filename: 'fonts/[name][ext]'
-        } 
+        }
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]'
-        } 
+        }
       }
-    ],
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-       favicon: path.join(__dirname, "src", "images/favicon.ico")
+      template: path.join(__dirname, 'src', 'index.html'),
+      favicon: path.join(__dirname, 'src', 'images/favicon.ico')
     }),
     new webpack.DefinePlugin({
-      "process.env.REACT_APP": JSON.stringify(process.env.REACT_APP),
-      "process.env.NODE_DEBUG": JSON.stringify(process.env.NODE_DEBUG),
+      'process.env.REACT_APP': JSON.stringify(process.env.REACT_APP),
+      'process.env.NODE_DEBUG': JSON.stringify(process.env.NODE_DEBUG)
     }),
     new webpack.ProvidePlugin({
-      process: "process/browser",
+      process: 'process/browser'
     }),
     new webpack.EnvironmentPlugin({
       BACKEND_API_URL:
         JSON.stringify(process.env.BACKEND_API_URL) ||
-        "https://api.test.saedi.io",
-      BUILD_INFO: `build: ${dateInfo}#${versionInfo}`,
+        'https://api.test.saedi.io',
+      BUILD_INFO: `build: ${dateInfo}#${versionInfo}`
     }),
-    //new BundleAnalyzerPlugin()
-  ],
-};
+    //new BundleAnalyzerPlugin(),
+    new CompressionPlugin({
+      algorithm: 'gzip'
+    })
+  ]
+}
